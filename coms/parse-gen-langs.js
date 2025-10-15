9d576f29c14123ab7176bdc8559e7322c6017f1c
franck gaspoz <franck.gaspoz@gmail.com>
2025-10-15 14:24:47 +0200

    first commit

---
+++ coms/parse-gen-langs.js
@@ -0,0 +1,13 @@
+// parse gen langs
+// db.podcast langs groups export -> normalized langs groups
+
+export default class ParseGenLangs {
+
+    gl = []
+    langKeys = []
+
+    test() {
+        console.log('TEST')
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,58 @@
+// js-tools CLI
+console.clear()
+
+import chalk from 'chalk'
+
+import ParseGenLangs from './coms/parse-gen-langs.js'
+const langs = (await import('./data/db-podcast-export-languages.js')).default
+//langs.default()
+
+console.log('loaded: db-podcast-export-languages')
+console.log('langs count: ' + langs.length)
+
+/*
+const low = await import('lowdb')
+const FileSync = await import('lowdb/adapters/FileSync')
+console.log(FileSync)
+const adapter = new FileSync('./data/iso-639-2.json')
+const iso6392 = low(adapter)
+*/
+
+import isoLangs from './data/iso-639-2.json' assert {type: 'json'};
+console.log('loaded: ISO-639-2. langs count:' + Object.keys(isoLangs).length)
+
+const args = process.argv
+const commands = ['parse-gen-langs']
+
+const usage = function () {
+  const usageText = `
+  js-tools helps you manage the wrpp datas sets
+
+  usage:
+    main <command>
+
+    commands can be:
+
+    parse-gen-langs :      db-podcast-export-languages -> ...
+  `
+  console.log(usageText)
+  process.exit(0)
+}
+
+// checks args
+if (args.length != 3) {
+  console.error('only one argument can be accepted. 1 argument required')
+  usage()
+}
+const com = args[2]
+if (!commands.includes(com)) {
+  console.error('unknown command: ' + com)
+  usage()
+}
+
+// run command
+switch (com) {
+  case 'parse-gen-langs':
+    const parseGenLangs = new ParseGenLangs()
+    break
+}
